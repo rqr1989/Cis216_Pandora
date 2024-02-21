d78a68ba4f0f8dab9ca6a7314d6bbb895bbb0d1d
rqr1989 <dontopenthepyxis@gmail.com>
2024-02-21 14:43:25 -0500

    Added menu music
    Added track for menu music. Added to each game menu set to play on awake and loop.

--- Pandora/Assets/Scripts/C_Moving_Platform.cs
+++ Pandora/Assets/Scripts/C_Moving_Platform.cs
@@ -2,12 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class C_Moving_Platform : MonoBehaviour
+public class CircularMovingPlatforms : MonoBehaviour
 {
+    [SerializeField] private Transform centerPoint;
+    [SerializeField] private float radius;
+    [SerializeField] private float speed;
+    [SerializeField] private float idleDuration;
 
+    private float currentAngle = 0f;
+    private float idleTimer = 0f;
 
- 
+    private void Update()
+    {
+        if (idleTimer >= idleDuration)
+        {
+            currentAngle += speed * Time.deltaTime;
+            if (currentAngle >= 360f)
+            {
+                currentAngle -= 360f;
+            }
+            idleTimer = 0f;
+        }
+        else
+        {
+            idleTimer += Time.deltaTime;
+        }
 
-
-
+        float newX = centerPoint.position.x + Mathf.Cos(currentAngle * Mathf.Deg2Rad) * radius;
+        float newZ = centerPoint.position.z + Mathf.Sin(currentAngle * Mathf.Deg2Rad) * radius;
+        transform.position = new Vector3(newX, transform.position.y, newZ);
     }
+}
